1735f1dd66f94b844d017a04d0ac8c673566ab7b
renatodellosso <54045197+renatodellosso@users.noreply.github.com>
2023-10-11 23:21:04 +0000

    Added check.sh

---
+++ check.sh
@@ -0,0 +1,11 @@
+symbol=$1
+
+echo "Checking $symbol..."
+
+binance="https://api.binance.us/api/v3/ticker/price?symbol=${symbol}USDT"
+echo "Curling $binance ..."
+curl $binance
+
+coinbase="https://api.coinbase.com/v2/prices/${symbol}-usd/spot"
+echo "Curling $binance ..."
+curl $binance
